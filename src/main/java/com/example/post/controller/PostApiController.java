2153043b50ed391c729ed871c24be660e12ba81a
sujeongHeo401 <gotnwjd40@gmail.com>
2022-03-05 22:52:55 +0900

    post 함수 파라미터에 객체를 넣어줌!

--- src/main/java/com/example/post/controller/PostApiController.java
+++ src/main/java/com/example/post/controller/PostApiController.java
@@ -1,5 +1,6 @@
 package com.example.post.controller;
 
+import com.example.post.dto.PostRequestDto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +12,8 @@ import java.util.Map;
 @RequestMapping("/api")
 public class PostApiController {
     @PostMapping("/post")
-    public void post(@RequestBody  Map<String, Object> requestData){
-        requestData.forEach((key, value) -> {
-            System.out.println("key : " + key);
-            System.out.println("key : " + value);
-        });
+    public void post(@RequestBody PostRequestDto requestData){
+        System.out.println(requestData);
 
     }
 }
